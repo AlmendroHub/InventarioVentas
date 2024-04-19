bd5ab75f3641697d6d99584f460d8ff8f35edfbc
AlmendroHub <paualmendro@outlook.com>
2024-04-19 12:20:17 +0200

    Agregar archivos de proyecto.

---
+++ Venta.cs
@@ -0,0 +1,13 @@
+﻿namespace InventarioVentas
+{
+    public class Venta
+    {
+        public int ID { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string NombreCliente { get; set; } = String.Empty;
+        public double? ValorProducto { get; set; }
+        public double? EfectivoRecibido { get; set; }
+        public double? Cambio { get; set; }
+        public int? Transferencia { get; set; } = 0;
+    }
+}
